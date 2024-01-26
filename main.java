5b5baabccb4b5e0fc23dc52f8b2d02f1d37ca2f7
Chuck476 <cdierbach@towson.edu>
2024-01-26 17:14:22 -0500

    Create main.java

---
+++ main.java
@@ -0,0 +1,14 @@
+// Keywords in Java
+
+public class Main {
+  
+  public static void main(String[] args) {
+    String class = "COSC237";
+    String meeting_time = "Tues/Thurs 9:30-10:45am";
+    String final = "Thursday December 14th, 8:00-10:00am";
+    
+    System.out.println("CLASS: " + class);
+    System.out.println("MEETING TIME: " + meeting_time);
+    System.out.println("FINAL EXAM DATE: " + final);
+  }
+}
