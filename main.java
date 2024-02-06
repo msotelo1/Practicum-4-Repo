1604bc7b2c6273069aab5147794e0bf1d2c728f7
msotelo1 <158190815+msotelo1@users.noreply.github.com>
2024-02-06 02:39:47 -0500

    Update main.java

--- main.java
+++ main.java
@@ -3,12 +3,12 @@
 public class Main {
   
   public static void main(String[] args) {
-    String class = "COSC237";
+    String class_name = "COSC237";
     String meeting_time = "Tues/Thurs 9:30-10:45am";
-    String final = "Thursday December 14th, 8:00-10:00am";
+    String final_date = "Thursday December 14th, 8:00-10:00am";
     
-    System.out.println("CLASS: " + class);
+    System.out.println("CLASS: " + class_name);
     System.out.println("MEETING TIME: " + meeting_time);
-    System.out.println("FINAL EXAM DATE: " + final);
+    System.out.println("FINAL EXAM DATE: " + final_date);
   }
 }
